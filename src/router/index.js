63decfbbb824ef198bf5bc4d4405bc12b4ebb737
themasterr09 <themasterr09@gmail.com>
2026-02-21 15:33:20 -0500

    Deploy

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('#'),
   routes,
   scrollBehavior: (to, from, savedPosition) => {
     // Restaurar posición al usar botón "atrás"
