3d9ffd46f3df3636e2ab938908f3601d1eb11078
themasterr09 <themasterr09@gmail.com>
2026-02-21 15:23:52 -0500

    base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+    base:'https://reyshadow0.github.io/catalogo-productos',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
